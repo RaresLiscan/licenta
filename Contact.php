2ac19b88f305a503369f84b24eadff24cf86953f
ShadowMurderer <catalin.vale5@gmail.com>
2022-06-09 21:48:23 +0300

    mi-o ajuns

--- Contact.php
+++ Contact.php
@@ -1,33 +1,27 @@
 ﻿<?php
 include("Conectare.php");
-$error='';
-if (isset($_POST['submit']))
-{
-// preluam datele de pe formular
-$name = htmlentities($_POST['name'], ENT_QUOTES);
-$email = htmlentities($_POST['email'], ENT_QUOTES);
-$phone = htmlentities($_POST['phone'], ENT_QUOTES);
-$message = htmlentities($_POST['message'], ENT_QUOTES);
+$error = '';
+if (isset($_POST['submit'])) {
+    // preluam datele de pe formular
+    $name = htmlentities($_POST['name'], ENT_QUOTES);
+    $email = htmlentities($_POST['email'], ENT_QUOTES);
+    $phone = htmlentities($_POST['phone'], ENT_QUOTES);
+    $message = htmlentities($_POST['message'], ENT_QUOTES);
 
-// verificam daca sunt completate
-if ($name == '' || $email == ''||$phone==''||$message=='')
-{
+    // verificam daca sunt completate
+    if ($name == '' || $email == '' || $phone == '' || $message == '') {
 
-$error = 'ERROR: Campuri goale!';
-} else {
+        $error = 'ERROR: Campuri goale!';
+    } else {
 
-if ($stmt = $mysqli->prepare("INSERT into messages (name, email, phone, message) VALUES (?, ?, ?, ?)"))
-{
-$stmt->bind_param("ssss", $name, $email,$phone,$message);
-$stmt->execute();
-$stmt->close();
-}
-
-else
-{
-echo "ERROR: Nu se poate executa insert.";
-}
-}
+        if ($stmt = $mysqli->prepare("INSERT into messages (name, email, phone, message) VALUES (?, ?, ?, ?)")) {
+            $stmt->bind_param("ssss", $name, $email, $phone, $message);
+            $stmt->execute();
+            $stmt->close();
+        } else {
+            echo "ERROR: Nu se poate executa insert.";
+        }
+    }
 }
 
 $mysqli->close();
@@ -35,12 +29,14 @@ $mysqli->close();
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Contact</title>
     <link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
     <!--menu bar-->
     <nav>
@@ -77,32 +73,38 @@ $mysqli->close();
                 </p>
                 <div class="info">
                     <div class="information">
-                        <img src="contact.png" class="icon-contactpage" alt="contact icon" />
-                        <p>Aici scriem locatia</p>
+                        <img src="poze/map-icon.png" class="icon-contactpage" alt="contact icon" />
+                        <p>Bul. Muncii, Cluj Napoca</p>
                     </div>
                     <div class="information">
-                        <img src="contact.png" class="icon-contactpage" alt="contact icon" />
-                        <p>Aici scriem email</p>
+                        <img src="poze/mail-icon.png" class="icon-contactpage" alt="contact icon" />
+                        <p>zoomin@gmail.com</p>
                     </div>
                     <div class="information">
-                        <img src="contact.png" class="icon-contactpage" alt="contact icon" />
-                        <p>Aici scriem nr de telefon</p>
+                        <img src="poze/phone-icon.png" class="icon-contactpage" alt="contact icon" />
+                        <p>+40714 837 290</p>
                     </div>
                 </div>
+                <p>Connect with us</p>
                 <div class="social-media">
-                    <p>Connect with us</p>
+
                     <div class="social-icons">
-                        <a href="#">
-                            <img src="contact.png" class="socials-contactpage" alt="contact icon" />
+
+                        <a href="#" class="social-links">
+                            <img src="poze/facebook-icon.png" class="socials-contactpage" alt="contact icon" width="35px" />
+
                         </a>
-                        <a href="#">
-                            <img src="contact.png" class="socials-contactpage" alt="contact icon" />
+                        <a href="#" class="social-links">
+                            <img src="poze/instagram-icon.png" class="socials-contactpage" alt="contact icon" width="35px" />
+
                         </a>
-                        <a href="#">
-                            <img src="contact.png" class="socials-contactpage" alt="contact icon" />
+                        <a href="#" class="social-links">
+                            <img src="poze/twitter-icon.png" class="socials-contactpage" alt="contact icon" width="40px" />
+
                         </a>
-                        <a href="#">
-                            <img src="contact.png" class="socials-contactpage" alt="contact icon" />
+                        <a href="#" class="social-links">
+                            <img src="poze/linked-icon.png" class="socials-contactpage" alt="contact icon" width="70px" />
+
                         </a>
                     </div>
                 </div>
@@ -139,4 +141,5 @@ $mysqli->close();
     </div>
     <script src="app.js"></script>
 </body>
+
 </html>
